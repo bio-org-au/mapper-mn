b6f4d65ac73d31f33022ca135ed482a36a2b7a7f
pmcneil <peter@nerderg.com>
2019-09-24 16:15:44 +1000

    Add license boilerplate to each file

--- src/test/groovy/au/org/biodiversity/mapper/BrokerControllerSpec.groovy
+++ src/test/groovy/au/org/biodiversity/mapper/BrokerControllerSpec.groovy
@@ -1,24 +1,33 @@
+/*
+    Copyright (c) 2019 Australian National Botanic Gardens and Authors
+
+    This file is part of National Species List project.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy
+    of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 package au.org.biodiversity.mapper
 
-import io.micronaut.http.client.DefaultHttpClientConfiguration
-import io.micronaut.http.client.HttpClientConfiguration
-import io.micronaut.http.client.exceptions.HttpClientResponseException
-import io.micronaut.runtime.ApplicationConfiguration
-
-import javax.inject.Named
-import javax.inject.Singleton
-
-import static io.micronaut.http.HttpRequest.*
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
 import io.reactivex.Flowable
 import spock.lang.Specification
 
 import javax.inject.Inject
+
+import static io.micronaut.http.HttpRequest.GET
 
 /**
  * User: pmcneil
