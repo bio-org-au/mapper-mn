c401c9d8824fa05221c6bd64cfb362c17465b23d
pmcneil <peter@nerderg.com>
2019-10-30 18:27:22 +1100

    more fiddling to connect to travis postgresql

--- src/test/resource/createTestDb.sh
+++ src/test/resource/createTestDb.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-dropdb --if-exists nsl-test && createdb nsl-test && psql -f ddl.sql nsl-test && psql -f test-data.sql nsl-test
+dropdb --if-exists nsl-test &&
+createdb nsl-test &&
+psql -f ddl.sql nsl-test &&
+psql -f test-data.sql nsl-test
