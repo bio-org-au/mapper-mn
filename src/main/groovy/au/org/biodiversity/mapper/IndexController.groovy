906cba593e1a52654f6aab6a8221bdbb588f899a
Mo <moziauddin@gmail.com>
2021-02-04 10:19:05 +1100

    Change the HTML on index page to plain text

--- src/main/groovy/au/org/biodiversity/mapper/IndexController.groovy
+++ src/main/groovy/au/org/biodiversity/mapper/IndexController.groovy
@@ -47,16 +47,15 @@ class IndexController {
     @Produces(MediaType.TEXT_HTML)
     @Get("/")
     HttpResponse index() {
-        HttpResponse.ok("""<html><body>
-<h1>Mapper</h1>
-<p>
+        HttpResponse.ok("""Mapper
+
 This is the mapper. It redirects you to resources. If you were using a resolvable URL then you should have been redirected,
 and we have a configuration problem. (all resolvable URLs should be pointed at /broker/ context path). 
-</p>
-<p>The mapper has a JSON API, this is the only HTML page</p> 
-<p>You can find mapper statistics at <a href="api/stats">stats</a></p>
-<p>Connected to DB ${dbUrl}</p>
-<p>Config files ${configFiles}
-</body></html>""")
+
+The mapper has a JSON API, this is the only HTML page.
+You can find mapper statistics at "api/stats"
+Connected to DB ${dbUrl}
+Config files ${configFiles}
+""")
     }
 }
